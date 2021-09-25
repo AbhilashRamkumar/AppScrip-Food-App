283dcaf7d7bee2ade68c273067ac57bbdb3f3c21
AbhilashRamkumar <47654947+AbhilashRamkumar@users.noreply.github.com>
2021-09-25 13:08:19 +0530

    Add files via upload
    First Upload

---
+++ AppscripHotel/Shared/AppscripHotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppscripHotelApp.swift
+//  Shared
+//
+//  Created by abc on 24/09/21.
+//
+
+import SwiftUI
+
+@main
+struct AppscripHotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
